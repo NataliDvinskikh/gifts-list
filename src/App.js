ac710fbf929cd6e0bcae628134f8e01a5b5acb52
Natali Dvinskikh <dvinskikhnatali@gmail.com>
2023-01-30 21:34:29 +0300

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+
+  const [gifts,setGifts] = useState(data);
+
+  const removeGift = (id) => {
+   let newGifts = gifts.filter(gift => gift.id !== id);
+   setGifts(newGifts);
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <h1> List of {gifts.length} wishes</h1>
+      </div>
+      {gifts.map((element => {
+        const {id,gift,image} = element;
+        return (
+          <div key={id}>
+            <div className='container'>
+              <h2>{id}-{gift}</h2>
+            </div>
+            <div className='container'>
+              <img src ={image} width = "300px" alt="phot"/>
+            </div>
+            <div className='container'>
+              <button onClick={() => removeGift (id)}>REMOVE</button>
+            </div>
+          </div>
+        )
+      }))}
+      <div className='container'>
+      <button onClick={()=> setGifts([])}>DELETE ALL</button>
+      </div>
     </div>
   );
 }
 
+
 export default App;
+
+
+
+
+
+
+// const removeGift = (id) => {
+//   // console.log(id) проверяем, что находится по id. Т.е. если мы тут id прописали nClick={() => removeGift (id), то и здесь обязательно const removeGift = (id) =>
+//      let newGifts = gifts.filter(gift => gift.id !== id); //id по которому мы кликаем не равно этому id то все поместить в новый массив, т.Е. если у товара 1 id=1, тогда не добавляем в новый массив
+//      setGifts(newGifts);
+//   }
---
+++ src/data.js
@@ -0,0 +1,56 @@
+export const data = [
+
+  {
+
+    id: 1,
+
+    gift: "Sunset",
+
+    image: "https://images.unsplash.com/photo-1541344302578-0ea37e27491d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+
+
+  },
+
+  {
+
+    id: 2,
+
+    gift: "Candels",
+
+    image: "https://images.unsplash.com/photo-1612179543058-ab74d388e0ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+
+  },
+
+  {
+
+    id: 3,
+
+    gift: "Jo Malone",
+
+    image: "https://images.unsplash.com/photo-1664198891866-8a35b73bb95f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+
+  },
+
+
+
+  {
+
+    id: 4,
+
+    gift: "Dishes",
+
+    image: "https://images.unsplash.com/photo-1632996547863-828cf385e4cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+
+  },
+
+  {
+
+    id: 5,
+
+    gift: "Tattoo",
+
+    image: "https://images.unsplash.com/photo-1655048956307-ff2d5769df3c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80"
+
+  }
+
+]
